4fd35470f3e6b71c4f4d332e793b06067d057143
Siddartha <sid.errabolu@gmail.com>
2019-08-28 03:19:00 -0400

    JWT Token

--- core/src/main/scala/org/bheaver/ngl4/aa/authentication/AuthenticationService.scala
+++ core/src/main/scala/org/bheaver/ngl4/aa/authentication/AuthenticationService.scala
@@ -1,6 +1,7 @@
 package org.bheaver.ngl4.aa.authentication
 
 import com.mongodb.client.model.Filters
+import com.typesafe.scalalogging.Logger
 import org.bheaver.ngl4.aa.authentication.datastore.{PatronDS, PatronDSImpl}
 import org.bheaver.ngl4.aa.authentication.exceptions.{AuthenticationFailureException, BadRequestException}
 import org.bheaver.ngl4.aa.db.DBConnection
@@ -17,15 +18,17 @@ trait AuthenticationService {
 }
 
 class AuthenticationServiceImpl extends AuthenticationService {
+  val logger = Logger(classOf[AuthenticationServiceImpl])
+  val jwtService: JWTService = new JWTServiceImpl
   override def authenticate(authenticationRequest: AuthenticationRequest): Future[AuthenticationSuccessResponse] = {
     implicit val mongoDatabase:MongoDatabase = DBConnection(authenticationRequest.libCode)
     val patronDS: PatronDS = new PatronDSImpl
     patronDS.findByCredentials(authenticationRequest.userName,authenticationRequest.password)
       .map(documentOpt => {
         documentOpt match {
-          case Some(value) =>new AuthenticationSuccessResponse("","PatId","fname","mname","lname","dept","patcat")
+          case Some(value) =>new AuthenticationSuccessResponse("",value.getString("patron_id"),value.getString("fname"),value.getString("mname"),value.getString("lname"),"dept","patcat")
           case None =>throw new AuthenticationFailureException("Authentication failed")
         }
-      })
+      }).map(authResponse => authResponse.copy(jwtToken = jwtService.encode(EncodeRequest(authResponse.patronId,authenticationRequest.libCode))))
   }
 }
---
+++ core/src/main/scala/org/bheaver/ngl4/aa/authentication/JWTService.scala
@@ -0,0 +1,74 @@
+package org.bheaver.ngl4.aa.authentication
+
+import java.time.Clock
+import java.util.Calendar
+
+import com.typesafe.scalalogging.Logger
+import pdi.jwt.{JwtAlgorithm, JwtJson4s}
+import org.json4s._
+import org.json4s.JsonDSL.WithBigDecimal._
+import org.json4s.jackson.JsonMethods._
+import pdi.jwt.{JwtAlgorithm, JwtJson4s}
+import org.json4s._
+import org.json4s.JsonDSL.WithBigDecimal._
+import org.json4s.jackson.JsonMethods._
+
+import scala.util.{Failure, Success}
+
+
+case class EncodeRequest(patronId: String, libCode: String)
+
+case class DecodeRequest(jwtToken: String, patronId: String, libCode: String)
+
+trait JWTService {
+  def encode(encodeRequest: EncodeRequest): String
+
+  def isJWTValid(decodeRequest: DecodeRequest): Boolean
+
+  def renewToken(decodeRequest: DecodeRequest): Option[String]
+}
+
+class JWTServiceImpl extends JWTService {
+  implicit val formats = DefaultFormats
+  val logger = Logger(classOf[JWTServiceImpl])
+  val iss = "Bheaver Inc"
+  val aud = "NGL4 App"
+  val secretKey = "somethingWeDontKnow"
+  val algo = JwtAlgorithm.HS256
+
+  override def encode(encodeRequest: EncodeRequest): String = {
+    val iat = Calendar.getInstance().getTimeInMillis
+    val exp = iat + (30 * 60 * 1000)
+    val claim = JObject(("iss", iss), ("aud", aud), ("iat", iat), ("exp", exp), ("patronId", encodeRequest.patronId), ("libCode", encodeRequest.libCode))
+    JwtJson4s.encode(claim, secretKey, algo)
+  }
+
+  override def isJWTValid(decodeRequest: DecodeRequest): Boolean = {
+    JwtJson4s.decodeAll(decodeRequest.jwtToken, secretKey, Seq(algo)) match {
+
+      case Failure(exception) => false
+
+      case Success(value) => {
+        val value1 = parse(value._2.content).extract[Map[String, String]]
+        value1("patronId").equals(decodeRequest.patronId) &&
+          value1("libCode").equals(decodeRequest.libCode) &&
+          value._2.issuer.getOrElse("").equals(iss) &&
+          value._2.audience.map(audSet => audSet.contains(aud)).getOrElse(false) &&
+          value._2.expiration.map(expriationTime => Calendar.getInstance().getTimeInMillis < expriationTime).getOrElse(false)
+      }
+
+    }
+  }
+
+  override def renewToken(decodeRequest: DecodeRequest): Option[String] = {
+    implicit val clock = Clock.systemUTC()
+    Option(decodeRequest)
+      .map(decodeRequestIn => isJWTValid(decodeRequestIn))
+      .map(isValid => if(isValid) {
+        JwtJson4s.decodeAll(decodeRequest.jwtToken, secretKey, Seq(algo)) match {
+          case Failure(exception) => null
+          case Success(value) =>JwtJson4s.encode(value._2.expiresIn(30 * 60 ))
+        }
+      }else null)
+  }
+}
