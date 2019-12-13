a6dcefdef151ede74ac294a5045f00427f0c7ca7
Sid <siddartha.errabolu@gmail.com>
2019-12-13 05:49:49 -0500

    Converetd JWTService to akka http

--- core/src/main/scala/org/bheaver/ngl4/aa/AAAkkaApp.scala
+++ core/src/main/scala/org/bheaver/ngl4/aa/AAAkkaApp.scala
@@ -5,8 +5,11 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
+import akka.http.scaladsl.server.PathMatcher
 import akka.stream.ActorMaterializer
 import org.bheaver.ngl4.aa.conf.AAServiceRegistry
+import org.bheaver.ngl4.aa.protocol.authentication.JWTService
+import org.bheaver.ngl4.aa.protocol.model.{DecodeRequest, JWTRenewTokenResponse}
 //import org.bheaver.ngl4.aa.authentication.datastore.PatronDSImpl
 import org.bheaver.ngl4.aa.authentication.exceptions.AuthenticationFailureException
 import org.bheaver.ngl4.aa.authentication.{AuthenticationRequest, AuthenticationService, /*AuthenticationServiceImpl,*/ AuthenticationSuccessResponse}
@@ -22,13 +25,17 @@ object AAAkkaApp extends App {
 
   //val authenticationService:AuthenticationService = new AuthenticationServiceImpl(new JWTServiceImpl,new PatronDSImpl(DBConnection(pureconfig.loadConfig[NGLConfig].right.get.dbSettings)))
   val authenticationService:AuthenticationService = AAServiceRegistry.authenticateService
+  val jwtService:JWTService = AAServiceRegistry.jwtService
   implicit val system = ActorSystem("AASystem")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   implicit val itemFormat = jsonFormat10(AuthenticationSuccessResponse)
   implicit val itemFormat1 = jsonFormat4(AuthenticationRequest)
-  val route = path("hello"){
+  implicit  val decodeRequestFormat = jsonFormat5(DecodeRequest)
+  implicit  val decodeResponseFormat = jsonFormat2(JWTRenewTokenResponse)
+  val pathMatcherAuth: PathMatcher[Unit] = "aa" / "v1" / "authenticate"
+  val routeAuth = path(pathMatcherAuth){
     get{
       entity(as[AuthenticationRequest]){ request =>
         val eventualResponse: Future[AuthenticationSuccessResponse] = authenticationService.authenticate(request)
@@ -42,5 +49,14 @@ object AAAkkaApp extends App {
       }
     }
   }
-  val bindingFuture = Http().bindAndHandle(route,"localhost")
+  val pathMatcherJWT:PathMatcher[Unit] = "aa" / "v1" / "renewToken"
+  val routeJWT = path(pathMatcherJWT) {
+    get{
+      entity(as[DecodeRequest]){ request =>
+        val eventualResponse = jwtService.renewToken(request)
+        complete(eventualResponse)
+      }
+    }
+  }
+  val bindingFuture = Http().bindAndHandle(concat(routeAuth,routeJWT),"localhost")
 }
--- protocol/src/main/scala/org/bheaver/ngl4/aa/protocol/model/Authorize.scala
+++ protocol/src/main/scala/org/bheaver/ngl4/aa/protocol/model/Authorize.scala
@@ -1,5 +1,5 @@
 package org.bheaver.ngl4.aa.protocol.model
 
-case class DecodeRequest(jwtToken: String, patronId: String, libCode: String, renewToken: Boolean = true, requestId: String = null)
+final case class DecodeRequest(jwtToken: String, patronId: String, libCode: String, renewToken: Boolean = true, requestId: String = null)
 
-case class JWTRenewTokenResponse(token: String, requestId: String)
+final case class JWTRenewTokenResponse(token: String, requestId: String)
